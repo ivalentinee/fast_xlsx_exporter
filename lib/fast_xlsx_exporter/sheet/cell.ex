1581de68bc766704a3fc1820084d0bfaeac18d1f
V. Ionov <valentine.emperor@gmail.com>
2020-02-17 14:27:02 +0400

    Rendering date

--- lib/fast_xlsx_exporter/sheet/cell.ex
+++ lib/fast_xlsx_exporter/sheet/cell.ex
@@ -9,6 +9,9 @@ defmodule FastXlsxExporter.Sheet.Cell do
   def render(id, {value, _}) when is_number(value), do: render_number(id, value)
   def render(id, {value, _}) when is_binary(value), do: render_string(id, value)
 
+  def render(id, {%Date{} = value, _}),
+    do: render_date(id, DateTimeFormatter.format_date(value))
+
   def render(id, {%NaiveDateTime{} = value, _}),
     do: render_datetime(id, DateTimeFormatter.format_datetime(value))
 
@@ -17,6 +20,11 @@ defmodule FastXlsxExporter.Sheet.Cell do
   EEx.function_from_file(:defp, :render_percent, "#{__DIR__}/cells/percent.xml.eex", [:id, :value])
 
   EEx.function_from_file(:defp, :render_string, "#{__DIR__}/cells/string.xml.eex", [:id, :value])
+
+  EEx.function_from_file(:defp, :render_date, "#{__DIR__}/cells/date.xml.eex", [
+    :id,
+    :value
+  ])
 
   EEx.function_from_file(:defp, :render_datetime, "#{__DIR__}/cells/datetime.xml.eex", [
     :id,
--- lib/fast_xlsx_exporter/sheet/datetime_formatter.ex
+++ lib/fast_xlsx_exporter/sheet/datetime_formatter.ex
@@ -5,17 +5,22 @@ defmodule FastXlsxExporter.Sheet.DateTimeFormatter do
   @initial_time ~T[00:00:00]
   @seconds_per_day 86400
 
+  def format_date(%Date{} = value) do
+    date_chunk = Date.diff(value, @initial_date)
+
+    if lotus_leap_year_bug?(date_chunk) do
+      2 + date_chunk
+    else
+      1 + date_chunk
+    end
+  end
+
   def format_datetime(%NaiveDateTime{} = value) do
     date = NaiveDateTime.to_date(value)
-    date_chunk = Date.diff(date, @initial_date)
     time = Time.diff(NaiveDateTime.to_time(value), @initial_time)
     time_chunk = time / @seconds_per_day
 
-    if lotus_leap_year_bug?(date_chunk) do
-      2 + date_chunk + time_chunk
-    else
-      1 + date_chunk + time_chunk
-    end
+    format_date(date) + time_chunk
   end
 
   defp lotus_leap_year_bug?(date_chunk), do: date_chunk >= 59
